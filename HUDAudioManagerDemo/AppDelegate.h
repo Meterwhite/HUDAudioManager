7c4ca88276edb5a3e2a0761c7c1161f14d11f6ae
Meterwhite <quxingyi@outlook.com>
2020-11-01 11:08:33 +0800

    Initial Commit

---
+++ HUDAudioManagerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HUDAudioManagerDemo
+//
+//  Created by MeterWhite on 2020/11/1.
+//  Copyright © 2020 Meterwhite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ HUDAudioManagerDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HUDAudioManagerDemo
+//
+//  Created by MeterWhite on 2020/11/1.
+//  Copyright © 2020 Meterwhite. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
